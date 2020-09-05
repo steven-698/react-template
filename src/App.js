5932c4ff61bf4d1247f11efd5e53d7a9259adde0
steven-698 <59348889+steven-698@users.noreply.github.com>
2020-09-05 14:49:46 +0700

    Add State for Follow Button

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
 
-export default function App() {
-  const username = "instagram";
+const Profile = (props) => {
+  const [isFollowed, setIsFollowed] = useState(false);
 
   return (
     <>
-      <img src="https://via.placeholder.com/300" alt="profile picture" />
-      <div>@{username}</div>
-      <button>Follow</button>
+      <img src="https://via.placeholder.com/150" alt="profile picture" />
+      <div>@{props.username}</div>
+      <button onClick={() => setIsFollowed(!isFollowed)}>
+        {isFollowed ? "Unfollow" : "Follow"}
+      </button>
       <div>Posts</div>
       <div>Follower</div>
       <div>Following</div>
       <div>Bio</div>
+      <Post src="https://via.placeholder.com/300" />;
     </>
   );
+};
+
+const Post = (props) => (
+  <>
+    <img src={props.src} alt="profile-picture" />
+  </>
+);
+
+export default function App() {
+  const username = "instagram";
+  return <Profile username={username} />;
 }
