75148d9c35051d706fff08dff833aeae5cac1f0c
steven-698 <59348889+steven-698@users.noreply.github.com>
2020-09-05 14:23:18 +0700

    Add elements - username, posts, follower, following, bio

--- src/App.js
+++ src/App.js
@@ -2,10 +2,17 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
+  const username = "instagram";
+
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <>
+      <img src="https://via.placeholder.com/300" alt="profile picture" />
+      <div>@{username}</div>
+      <button>Follow</button>
+      <div>Posts</div>
+      <div>Follower</div>
+      <div>Following</div>
+      <div>Bio</div>
+    </>
   );
 }
