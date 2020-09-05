8a3d14fe38dac43e369200646a66bb1d15709245
steven-698 <59348889+steven-698@users.noreply.github.com>
2020-09-05 15:38:50 +0700

    Add Css Grid

--- src/App.js
+++ src/App.js
@@ -10,11 +10,19 @@ const Post = ({ src }) => (
 const Profile = (props) => {
   const { username } = props;
   const [isFollowed, setIsFollowed] = useState(false);
-  const postCount = 0;
+  const [queryString, setQueryString] = useState("");
+  const postCount = 6;
   const posts = [...Array(postCount)];
 
   return (
     <>
+      <input
+        onChange={(e) => {
+          setQueryString(e.target.value);
+        }}
+        placeholder="search profile"
+      />
+      <div>Search Result for Account : {queryString}</div>
       <img src="https://via.placeholder.com/150" alt="profile picture" />
       <div>@{username}</div>
       <button onClick={() => setIsFollowed(!isFollowed)}>
@@ -27,9 +35,15 @@ const Profile = (props) => {
       {postCount === 0 ? (
         <div>No Posts</div>
       ) : (
-        posts.map((_, idx) => (
-          <Post src="https://via.placeholder.com/300" key={idx} />
-        ))
+        <div className="posts">
+          {posts.map((_, idx) => (
+            <Post
+              src="https://via.placeholder.com/300"
+              key={idx}
+              className="post"
+            />
+          ))}
+        </div>
       )}
     </>
   );
